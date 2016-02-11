1ec6e0e810f4aa647e3f1a5fc21147d8ce72eb32
James Moulton <you@example.com>
2016-02-11 11:59:36 -0500

    added code for multiple rating buttons, currently blue squares

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -9,13 +9,48 @@
 import UIKit
 
 class RatingControl: UIView {
+    
+    //MARK: Properties
+    var rating = 0
+    var ratingButtons = [UIButton]()
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+    //MARK: Initialization
+    override func layoutSubviews() {
+        
+        //set button width/height to a square the size of the frame's height
+        let buttonSize = Int(frame.size.height)
+        
+        var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
+        
+        //offset each button by the length of the button plus spacing
+        for (index, button) in ratingButtons.enumerate() {
+            buttonFrame.origin.x = CGFloat(index * (buttonSize + 5))
+            button.frame = buttonFrame
+        }
     }
-    */
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        
+        super.init(coder: aDecoder)
+        
+        
+        for _ in 0..<5 {
+            let button = UIButton()
+            button.backgroundColor = UIColor.blueColor()
+            button.addTarget(self, action: "RatingButtonTapped:", forControlEvents:
+                .TouchDown)  //when user has pressed on a rating button
+            ratingButtons += [button]  //adding buttons to the array
+            addSubview(button)
+        }
+    }
+    
+    override func intrinsicContentSize() -> CGSize {
+        let buttonSize = Int(frame.size.height)
+        let width = (buttonSize + 5) 
+        return CGSize(width: width, height: buttonSize)
+    }
+    //MARK: Button Action
+    func RatingButtonTapped(button: UIButton) {
+        print("Button Pressed")
+    }
 }
