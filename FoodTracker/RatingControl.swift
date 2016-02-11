42ab476535007fe65a8b6286c453352983810ff9
James Moulton <you@example.com>
2016-02-11 09:37:59 -0500

    placing Food Tracker repo

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by James Moulton on 2/9/16.
+//  Copyright © 2016 lavaflowmobile. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
