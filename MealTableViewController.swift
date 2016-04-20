53d6eadb4cf0b5852099818559549966f389bef3
James Moulton <you@example.com>
2016-04-20 15:47:25 -0400

    - data persistence completed

--- MealTableViewController.swift
+++ MealTableViewController.swift
@@ -33,11 +33,15 @@ class MealTableViewController: UITableViewController {
         //use edit button item
         navigationItem.leftBarButtonItem = editButtonItem()
         
-        //load sample data
-        loadSampleMeals()
+        //load saved meals, otherwise load sample data
+        if let savedMeals = loadMeals() {
+            meals += savedMeals
+        } else {
+            //load sample data
+            loadSampleMeals()
+        }
         
-        
-            }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -82,6 +86,7 @@ class MealTableViewController: UITableViewController {
         if editingStyle == .Delete {
             // Delete the row from the data source
             meals.removeAtIndex(indexPath.row)
+            saveMeals()
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
@@ -140,6 +145,18 @@ class MealTableViewController: UITableViewController {
                     meals.append(meal)
                     tableView.insertRowsAtIndexPaths([newIndexPath], withRowAnimation: .Bottom)
                 }
+                //save meals
+                saveMeals()
         }
+    }
+    //MARK: NSCoding
+    func saveMeals() {
+        let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(meals, toFile: Meal.ArchiveURL.path!)
+            if !isSuccessfulSave {
+                print("failed to save meals...")
+        }
+    }
+    func loadMeals() -> [Meal]? {
+        return NSKeyedUnarchiver.unarchiveObjectWithFile(Meal.ArchiveURL.path!) as? [Meal]
     }
 }
